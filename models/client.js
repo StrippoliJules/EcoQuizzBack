5027cfea964965b8badd5a46878dff8ea4c06f32
unknown <couly.guilhem@gmail.com>
2024-11-04 10:37:00 +0100

    Fix: Models

--- models/client.js
+++ models/client.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 const ClientSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  phone: { type: String },
-  address: { type: String },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Client', ClientSchema);
--- models/invoice.js
+++ models/invoice.js
@@ -3,13 +3,12 @@ const mongoose = require('mongoose');
 
 const InvoiceSchema = new mongoose.Schema({
   client: { type: mongoose.Schema.Types.ObjectId, ref: 'Client', required: true },
+  address: {type:String ,required: true},
   items: [{
     description: { type: String, required: true },
     quantity: { type: Number, required: true },
     price: { type: Number, required: true },
   }],
-  total: { type: Number, required: true },
-  status: { type: String, enum: ['paid', 'unpaid'], default: 'unpaid' },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Invoice', InvoiceSchema);
